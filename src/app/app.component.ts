0e9ad15875f86283f01d8508f2d6936917db2598
kadirsenol <kdrsnl_61@hotmail.com>
2024-07-09 17:39:40 +0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  template: `
+  <p>asdasdasdqqqqq<p>
+  <div style="background-color: blue;"><app-home [titlea]="title"/></div> 
+  <input type="text" value={{title}}>
+  <input type="text" [value]="title">
+  <input type="text" [(ngModel)] = "title">
+  <input type="text" [(ngModel)] = "title">
+   
+  
+  `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'project1';
+  title : string = 'project1asd';
 }
+  
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+   @Input() titlea : string = '';
+
+   metot(): void{
+    console.log("hellooo");
+   }
+}
